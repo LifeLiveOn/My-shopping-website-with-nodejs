61ed9210d51f4f435487dd356fbdfaf3120e49d8
Thanh <tnkchaseme@gmail.com>
2021-10-08 08:44:31 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+var exphbs  = require('express-handlebars'); // it is like render_template from python
+var morgan = require('morgan')
+const app = express()
+const port = 3000
+//public folder với các file như img ...
+app.use(express.static(path.join(__dirname, 'public')));
+// mo f12 view console co bieu tuong cua nodejs xai do de debug tren trinh duyet - morgan 
+// http logger
+app.use(morgan('combined'))
+
+
+// use engine handle bar template
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views',path.join(__dirname,'resource\\views'));
+
+// home path, there is get, post, put, detele ex: app.put 
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+
+app.get('/news',(req,res)=>{
+  res.render('news')
+})
+
+
+app.get('/search',(req,res)=>{
+  res.render('search')
+})
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})

f26ce6c746b201ab0ad018d87da4f28bf82cdea9
Thanh <tnkchaseme@gmail.com>
2021-10-18 19:45:20 +0700

    adding index html

--- src/config/db/database.js
+++ src/config/db/database.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/toy_store_dev', {
+mongoose.connect(process.env.MongoDB_URL || 'mongodb://localhost:27017/toy_store_dev', {
     useNewUrlParser: true,
 });
+//mongodb+srv://tnkchaseme:23062002l@cluster0.a3acr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 var conn = mongoose.connection;
 conn.on('connected', function () {
     console.log('database is connected successfully');
